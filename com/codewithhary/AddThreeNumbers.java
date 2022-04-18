22ec1abfedf0266dc02fc53edb1e46c10d4ed483
geetc <geetc1919@gmail.com>
2022-04-18 19:59:41 +0530

    AddThreeNumbers by code with harry's practice set 1 it is the 1st one

---
+++ com/codewithhary/AddThreeNumbers.java
@@ -0,0 +1,22 @@
+package com.codewithhary;
+
+import java.util.Scanner;
+
+public class AddThreeNumbers {
+
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("enter three numbers");
+        System.out.println("enter A");
+        int a = sn.nextInt();
+        System.out.println("enter B");
+        int b = sn.nextInt();
+        System.out.println("enter C");
+        int c = sn.nextInt();
+        System.out.print("the sum of A+B+C: " + (a+b+c));
+
+         }
+
+
+}
